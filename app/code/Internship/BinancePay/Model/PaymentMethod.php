579769f3864c24c4aa3eb26014044190c747247d
tomk1v <tomkivandrii18@gmail.com>
2024-04-01 12:45:07 +0300

    add authorize method

--- app/code/Internship/BinancePay/Model/PaymentMethod.php
+++ app/code/Internship/BinancePay/Model/PaymentMethod.php
@@ -1,10 +1,15 @@
 <?php
+/**
+ * @category Internship
+ * @package Internship\BinancePay
+ * @author Andrii Tomkiv <tomkivandrii18@gmail.com>
+ * @copyright 2024 Tomkiv
+ */
+
+declare(strict_types=1);
 
 namespace Internship\BinancePay\Model;
 
-/**
- * Pay In Store payment method model
- */
 class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
 {
     /**
@@ -12,5 +17,47 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
      *
      * @var string
      */
-    protected $_code = 'custompayment';
+    protected $_code = 'binancepay';
+
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Framework\App\Response\Http $http,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = [],
+        \Magento\Directory\Helper\Data $directory = null
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $resource,
+            $resourceCollection,
+            $data,
+            $directory
+        );
+        $this->http = $http;
+    }
+
+    /**
+     * Authorize payment method
+     *
+     * @param \Magento\Payment\Model\InfoInterface $payment
+     * @param float $amount
+     * @return $this
+     */
+    public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
+        echo 1;
+    }
 }
